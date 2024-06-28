dcaecb831c587572f8e5dce1761169dacc6be024
Finn Fancher <fancherfinn@gmail.com>
2024-06-28 09:44:34 -0700

    update main.py

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ def main():
     ########################################
     """
     total = 0
+    i = 5
+    while(i < 5):
+        total += (int)(input("Enter number: "))
+        i += 1
+        
+    print(total)
 
     ########################################
     # Do not delete the return statement
